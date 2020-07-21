08fd8f498f0d0c88f8d8b33e540d3de7a6353348
Qinwei Gong <qinwei@sliver.tv>
2020-07-20 17:47:04 -0700

    cleanup

--- thetaetl/exporters.py
+++ thetaetl/exporters.py
@@ -167,8 +167,6 @@ class JsonLinesItemExporter(BaseItemExporter):
         self.encoder = JSONEncoder(**kwargs)
 
     def export_item(self, item):
-        print("=-=-=-=-=-=-==-=-=")
-        print(item)
         itemdict = dict(self._get_serialized_fields(item))
         data = self.encoder.encode(itemdict) + '\n'
         self.file.write(to_bytes(data, self.encoding))
